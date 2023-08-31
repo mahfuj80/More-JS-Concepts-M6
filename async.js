433fe93611d95b06bff2d6646b32f83767ad0533
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-08-31 15:48:32 +0600

    Single-threaded, Asynchronous vs Synchronous

---
+++ async.js
@@ -0,0 +1,17 @@
+console.log(1);
+console.log(2);
+setTimeout(() => {
+    console.log('laizy loged');
+}, 4000)
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     .then(response => response.json())
+//     .then(json => console.log(json))
+console.log(4);
+console.log(5);
+console.log(6);
+console.log(7);
+console.log(8);
+
+function doSomething() {
+    console.log(3);
+}
